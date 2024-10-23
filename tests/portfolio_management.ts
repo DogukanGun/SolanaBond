5254dd4ae9ececcfc9c477c6e35e0e05bebb1742
Atamert Rahma <atamertrahma@gmail.com>
2024-10-23 23:04:35 +0200

    add create test, update packagejson
    add pyth-solana-receiver ts sdk
    add expected values to the created bond

--- tests/portfolio_management.ts
+++ tests/portfolio_management.ts
@@ -2,6 +2,8 @@ import * as anchor from "@coral-xyz/anchor";
 import { BN, Program } from "@coral-xyz/anchor";
 import { PortfolioManagement } from "../target/types/portfolio_management";
 import { LAMPORTS_PER_SOL } from "@solana/web3.js";
+import { getPriceFeedAccountForProgram } from "@pythnetwork/pyth-solana-receiver"
+import { expect } from "chai";
 
 describe("portfolio_management", () => {
   // Configure the client to use the local cluster.
@@ -10,12 +12,14 @@ describe("portfolio_management", () => {
 
   const program = anchor.workspace.PortfolioManagement as Program<PortfolioManagement>;
   const maker = anchor.web3.Keypair.generate();
-  const [investersPDA, bump] = anchor.web3.PublicKey.findProgramAddressSync(
+  const [investorsPDA, bump] = anchor.web3.PublicKey.findProgramAddressSync(
     [
-      Buffer.from("investers"), // Constant seed
+      Buffer.from("investors"), // Constant seed
     ],
     program.programId
   );
+  const defaultShardId = 0;
+
   const confirm = async (signature: string): Promise<string> => {
     const block = await provider.connection.getLatestBlockhash();
     console.log(block.blockhash)
@@ -32,11 +36,12 @@ describe("portfolio_management", () => {
     const airdropMaker = await provider.connection.requestAirdrop(maker.publicKey, 20 * anchor.web3.LAMPORTS_PER_SOL).then(confirm);
     console.log("\nAirdropped 5 SOL to maker", airdropMaker);
   })
+
   it("Create Bond!", async () => {
-    // Add your test here.
+    let feedId = "0x63f341689d98a12ef60a5cff1d7f85c70a9e17bf1575f0e7c0b2512d48b1c8b3"
     const tx = await program
       .methods
-      .createBond("0x63f341689d98a12ef60a5cff1d7f85c70a9e17bf1575f0e7c0b2512d48b1c8b3")
+      .createBond(feedId)
       .accounts({
         payer: maker.publicKey
       })
@@ -44,6 +49,14 @@ describe("portfolio_management", () => {
       .rpc();
     confirm(tx);
     console.log("Your transaction signature", tx);
+
+    let investorsAccount = await program.account.investorsAccount.fetch(investorsPDA);
+    expect(getPriceFeedAccountForProgram(defaultShardId, Buffer.from(investorsAccount.feedId)))
+      .eql(getPriceFeedAccountForProgram(defaultShardId, feedId));
+    expect(investorsAccount.numInvestors).equal(0);
+    expect(investorsAccount.investors).to.be.an("array").that.is.empty;
+    expect(investorsAccount.tokenAddress.equals(anchor.web3.PublicKey.default)); // ones
+    expect(investorsAccount.investorsBump).equal(bump);
+    expect(investorsAccount.vaultBump).equal(0);
   });
-  
 });
