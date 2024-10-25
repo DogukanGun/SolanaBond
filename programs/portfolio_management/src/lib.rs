a94b39b9536a992e901349f8e70ca2880d304f36
Atamert Rahma <atamertrahma@gmail.com>
2024-10-25 23:01:38 +0200

    format functions

--- programs/portfolio_management/src/lib.rs
+++ programs/portfolio_management/src/lib.rs
@@ -11,15 +11,15 @@ declare_id!("91dxXHBrNHJQHkze1C8JuuTnE6nvg5r9Ltbs5NdD5MQZ");
 pub mod portfolio_management {
     use super::*;
 
-    pub fn create_bond(ctx: Context<CreateBond>,feed_id:String) -> Result<()> {
+    pub fn create_bond(ctx: Context<CreateBond>, feed_id: String) -> Result<()> {
         ctx.accounts.create_bond(feed_id,&ctx.bumps)
     }
 
-    pub fn invest_in_bond(ctx: Context<Fund>,amount:u64) -> Result<()> {
+    pub fn invest_in_bond(ctx: Context<Fund>, amount: u64) -> Result<()> {
         ctx.accounts.transfer_tokens(amount)
     }
 
-    pub fn trade(ctx: Context<Trade>,ethereum_price:f64) -> Result<()> {
+    pub fn trade(ctx: Context<Trade>, ethereum_price: f64) -> Result<()> {
         ctx.accounts.trade(ethereum_price)
     }
 }
