fe7aacd598ca5c66fc5efcc616ca10f233b672fa
Atamert Rahma <atamertrahma@gmail.com>
2024-11-02 13:30:22 +0100

    flatten module structure

--- programs/portfolio_management/src/token_bridge/mod.rs
+++ programs/portfolio_management/src/token_bridge/mod.rs
@@ -2,3 +2,8 @@ pub mod context;
 pub mod state;
 pub mod instructions;
 pub mod error;
+
+pub use context::*;
+pub use state::*;
+pub use instructions::*;
+pub use error::*;
--- programs/portfolio_management/src/token_bridge/state/mod.rs
+++ programs/portfolio_management/src/token_bridge/state/mod.rs
@@ -1,8 +1,7 @@
 pub mod foreign_contract;
-pub use foreign_contract::*;
-
 pub mod redeemer_config;
-pub use redeemer_config::*;
-
 pub mod sender_config;
+
+pub use foreign_contract::*;
+pub use redeemer_config::*;
 pub use sender_config::*;
