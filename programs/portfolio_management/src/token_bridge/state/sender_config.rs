155df0d36ab1d4a0fba572290b10204b5d576bb5
Atamert Rahma <atamertrahma@gmail.com>
2024-11-02 13:16:41 +0100

    remove custody signer, add seed prefix and test mem size

--- programs/portfolio_management/src/token_bridge/state/sender_config.rs
+++ programs/portfolio_management/src/token_bridge/state/sender_config.rs
@@ -6,7 +6,6 @@ pub struct OutboundTokenBridgeAddresses {
     /// PDAs (program addresses)
     pub config: Pubkey,
     pub authority_signer: Pubkey,
-    pub custody_signer: Pubkey,
     pub emitter: Pubkey,
     pub sequence: Pubkey,
     /// [BridgeData](wormhole_anchor_sdk::wormhole::BridgeData) address
@@ -19,7 +18,6 @@ impl OutboundTokenBridgeAddresses {
     pub const MAXIMUM_SIZE: usize =
             32      // config
         +   32      // authority_signer
-        +   32      // custody_signer
         +   32      // emitter
         +   32      // sequence
         +   32      // wormhole_bridge
@@ -38,7 +36,28 @@ impl SenderConfig {
     pub const MAXIMUM_SIZE: usize =
             32      // owner
         +   1       // bump
-        //+   OutboundTokenBridgeAddresses::MAXIMUM_SIZE  // token_bridge
+        +   OutboundTokenBridgeAddresses::MAXIMUM_SIZE  // token_bridge
         +   1       // finality
     ;
+
+    /// b"sender"
+    pub const SEED_PREFIX: &[u8; 6] = b"sender";
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::mem::size_of;
+
+    #[test]
+    fn test_sender_config_mem_size() {
+        assert_eq!(
+            OutboundTokenBridgeAddresses::MAXIMUM_SIZE,
+            size_of::<OutboundTokenBridgeAddresses>()
+        );
+        assert_eq!(
+            SenderConfig::MAXIMUM_SIZE,
+            size_of::<Pubkey>() + size_of::<u8>() + size_of::<OutboundTokenBridgeAddresses>() + size_of::<u8>()
+        );
+    }
 }
